c98ef49046e9f86ed9fa4e93480f9d9c3faaf4ab
Dmitriy Filyustin <midnight.88s@gmail.com>
2024-03-11 23:51:47 +0300

    Updated yandex open webinar tests

--- src/test/kotlin/yandex/interview/Open webinar tests.kt
+++ src/test/kotlin/yandex/interview/Open webinar tests.kt
@@ -1,7 +1,10 @@
 package yandex.interview
 
+import leet.yandex.TreeNode
 import org.junit.jupiter.api.Test
+import kotlin.math.max
 import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
 fun IntArray.swap(index1: Int, index2: Int) {
   val holder = get(index1)
@@ -17,6 +20,40 @@ fun IntArray.reverse() {
     i++
   }
 }
+
+fun TreeNode.leafSum(): Int {
+  fun TreeNode.findLeafAndSum(): Int {
+    if (left == null && right == null) {
+      return `val`
+    }
+    return (left?.findLeafAndSum() ?: 0) + (right?.findLeafAndSum() ?: 0)
+  }
+  return findLeafAndSum()
+}
+
+fun longestUniqueSubArraySize(array: IntArray): Int {
+  val acc = mutableSetOf<Int>()
+  var subArraySize = 0
+  var left = 0
+  var i = 0
+  while (i < array.size) {
+    if (acc.contains(array[i])) {
+      subArraySize = max(subArraySize, acc.size)
+
+      var foundNewLeft = false
+      while(!foundNewLeft) {
+        if(array[left] == array[i]) {
+          foundNewLeft = true
+        }
+        acc.remove(array[left++])
+      }
+    }
+    acc.add(array[i])
+    i++
+  }
+  return max(subArraySize, acc.size)
+}
+
 
 /**
  * https://www.youtube.com/watch?v=aYuAd-IDigc
@@ -38,7 +75,60 @@ class OpenWebinarTests {
       intArrayOf(),
       intArrayOf().apply { reverse() }
     )
+  }
 
+  @Test
+  fun `test leafSum`() {
+    assertEquals(
+      1,
+      TreeNode(1).leafSum()
+    )
+
+    assertEquals(
+      3,
+      TreeNode(1).apply {
+        left = TreeNode(3)
+      }.leafSum()
+    )
+
+    assertEquals(
+      8,
+      TreeNode(1).apply {
+        left = TreeNode(3)
+        right = TreeNode(5)
+      }.leafSum()
+    )
+
+    assertEquals(
+      10,
+      TreeNode(1).apply {
+        left = TreeNode(3).apply {
+          left = TreeNode(5)
+        }
+        right = TreeNode(5)
+      }.leafSum()
+    )
+  }
+
+  @Test
+  fun `test longestUniqueSubArraySize`() {
+    assertEquals(
+      3,
+      longestUniqueSubArraySize(intArrayOf(1, 2, 3))
+    )
+    assertEquals(
+      3,
+      longestUniqueSubArraySize(intArrayOf(1, 2, 3, 3, 3, 3))
+    )
+    assertEquals(
+      5,
+      longestUniqueSubArraySize(intArrayOf(1, 2, 3, 3, 3, 4, 5, 6, 7))
+    )
+
+    assertEquals(
+      7,
+      longestUniqueSubArraySize(intArrayOf(1, 2, 3, 5, 2, 4, 7, 8, 9))
+    )
 
   }
 }
