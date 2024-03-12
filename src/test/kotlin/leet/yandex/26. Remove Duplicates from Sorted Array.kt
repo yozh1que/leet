4e22a5d0f1bc3c5024d1c0ae773cf0f3d444363a
Dmitriy Filyustin <midnight.88s@gmail.com>
2024-03-12 18:26:41 +0300

    upd 26. Remove Duplicates from Sorted Array

--- src/test/kotlin/leet/yandex/26. Remove Duplicates from Sorted Array.kt
+++ src/test/kotlin/leet/yandex/26. Remove Duplicates from Sorted Array.kt
@@ -1,19 +1,18 @@
 package leet.yandex
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 fun removeDuplicates(nums: IntArray): Int {
-  var k = 0
-  var uniqueElementIndex = 0
-  var i = 0
-  val last = nums[nums.size - 1]
+  if (nums.isEmpty()) return 0
+  var k = 1
+  var i = 1
   while (i < nums.size) {
-    if (i == nums.size - 1 || nums[i] != nums[i + 1]) {
+    if(nums[k-1] != nums[i]) {
+      nums[k] = nums[i]
       k++
-      nums[uniqueElementIndex++] = nums[i]
     }
-    if (last == nums[i]) return k + 1
     i++
   }
   return k
@@ -22,10 +21,14 @@ fun removeDuplicates(nums: IntArray): Int {
 class RemoveDuplicatesFromSortedArrayTest {
   @Test
   fun `test removeDuplicates`() {
-    var arr = intArrayOf(1, 1, 2)
+    var arr = intArrayOf(0, 1, 1, 1, 2, 3)
     assertEquals(
-      2,
+      4,
       removeDuplicates(arr)
+    )
+    assertContentEquals(
+      intArrayOf(0, 1, 2, 3, 2, 3),
+      arr
     )
     println(arr.joinToString())
 
@@ -33,6 +36,10 @@ class RemoveDuplicatesFromSortedArrayTest {
     assertEquals(
       5,
       removeDuplicates(arr)
+    )
+    assertContentEquals(
+      intArrayOf(0, 1, 2, 3, 4, 2, 2, 3, 3, 4),
+      arr
     )
 
     println(arr.joinToString())
